9935c56b01debe422110580b9e89eabce454f30a
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:40:36 -0300

    Create exponenciacao.py

---
+++ exponenciacao.py
@@ -0,0 +1,8 @@
+# Faça um programa em Python que realize a exponenciação de um valor 10x.
+i=0
+while i<=10:
+    print(2**i)
+    i = i+1
+print("Fim")
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
